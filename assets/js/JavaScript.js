58977fa3dec01b82d580e1d4fb5f57c9d936adeb
Bennat Jose <bennatjose@gmail.com>
2014-10-25 18:19:41 -0600

    mousewheel working

---
+++ assets/js/JavaScript.js
@@ -0,0 +1,81 @@
+﻿$(document).ready(function () {
+    $("#projects").hide();
+    $("#about").hide();
+    $("#contact").hide();
+
+    $('#home').mousewheel(function (event, delta) {
+        event.preventDefault(); // if you want to prevent the window from scrolling
+
+        if (delta < 0) {
+            $(this).hide();
+            $("#projects").show();
+
+
+        }
+        else {
+            "";
+
+
+        }
+    });
+
+    $('#projects').mousewheel(function (event, delta) {
+        event.preventDefault(); // if you want to prevent the window from scrolling
+
+        if (delta < 0) {
+            $(this).hide();
+            $("#about").show();
+
+        }
+        else {
+
+            $(this).hide();
+            $("#home").show();
+
+
+        }
+    });
+
+    $('#about').mousewheel(function (event, delta) {
+        event.preventDefault(); // if you want to prevent the window from scrolling
+
+        if (delta < 0) {
+
+            $(this).hide();
+            $("#contact").show();
+
+
+        }
+        else {
+
+            $(this).hide();
+            $("#projects").show();
+
+
+        }
+    });
+
+    $('#contact').mousewheel(function (event, delta) {
+        event.preventDefault(); // if you want to prevent the window from scrolling
+
+        if (delta < 0) {
+
+
+            $(this).hide();
+            $('#home').show();
+
+        }
+        else {
+
+            $(this).hide();
+            $('#about').show();
+
+
+        }
+    });
+
+
+
+
+
+});
